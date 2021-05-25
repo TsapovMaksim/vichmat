3b8d422654cc9b95b0f191141cfbbc7ffec4c87a
Maxim <tsapov552@gmail.com>
2021-05-25 17:26:08 +0700

    Ппытка

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 // import * as math from 'mathjs';
 
 function App() {
-  const x0 = 0,
-    y0 = 1,
-    y1 = 1;
+  const x0 = 1,
+    y0 = 2,
+    y1 = 3;
   const h = 0.5;
-  const range = [0, 1];
+  const range = [1, 2];
   const eps = 10 ** -6;
 
   const rengeKytta2 = () => {
@@ -19,11 +19,14 @@ function App() {
     let i = 0;
     let answer;
     const finalTable = [];
+    finalTable.push({ x: x0, y: y0, "y'": y1 });
 
     while (true) {
       i++;
+      y1Answers.length = 0;
+      yAnswers.length = 0;
 
-      for (let i = range[0], j = 0; +i.toFixed(16) < range[1]; i += newH, j++) {
+      for (let i = range[0], j = 0; +i.toFixed(8) < range[1]; i += newH, j++) {
         let y = getY(xArray[j], yArray[j], newH);
         yArray.push(y);
         xArray.push(xArray[j] + newH);
@@ -33,51 +36,63 @@ function App() {
       yArray = [[y0, y1]];
       xArray = [x0];
 
-      for (let i = range[0], j = 0; +i.toFixed(16) < range[1]; i += newH / 2, j++) {
-        let y = getY(xArray[j], yArray[j], newH);
+      for (let i = range[0], j = 0; +i.toFixed(8) < range[1]; i += newH / 2, j++) {
+        let y = getY(xArray[j], yArray[j], newH / 2);
         yArray.push(y);
-        xArray.push(xArray[j] + newH);
-        y1Answers[j] = y;
+        xArray.push(xArray[j] + newH / 2);
+        y1Answers.push(y);
       }
+
+      yArray = [[y0, y1]];
+      xArray = [x0];
 
       let isEps = checkEps(yAnswers, y1Answers, eps);
 
       if (isEps.flag) {
-        answer = isEps.answer;
         break;
       }
 
       newH = newH / 2;
     }
 
-    for (let i = 0, x = x0; i < yAnswers.length; i++, x += newH) {
-      finalTable[i] = { x: x, y: yAnswers[i][0], "y'": yAnswers[i][1] };
+    for (let i = 0, x = 0; i < yAnswers.length; i++, x += newH) {
+      finalTable.push({ x: +(x + newH).toFixed(8), y: yAnswers[i][0], "y'": yAnswers[i][1] });
     }
 
-    // console.log('Y: ', yAnswers);
-    // console.log('Y: ', finalTable);
-    // console.log('Y1: ', y1Answers);
     console.log('Шаг: ', newH);
     console.log('Кол-во: ', i);
     console.log(finalTable);
     // console.table(finalTable);
 
+    // let diff = [];
+    // for (let i = 0; i < yAnswers.length; i++) {
+    //   diff.push({
+    //     diff: Math.abs(yAnswers[i][0] - y1Answers[i][0]),
+    //     isEps:
+    //       Math.abs(yAnswers[i][0] - y1Answers[i][0]) < eps * 3 ||
+    //       Math.abs(yAnswers[i][1] - y1Answers[i][1]) < eps * 3,
+    //   });
+    // }
+    // console.log(diff.filter(el => el.isEps));
+
     return answer;
   };
 
   function checkEps(answers, answers1, eps) {
-    console.log('Compare');
     for (let i = 1; i < answers.length; i++) {
-      if (Math.abs(answers[i][0] - answers1[i * 2][0]) >= eps * 3) {
-        return { flag: false };
-      } else {
-        return { flag: true, answer: answers1[i * 2][0] };
+      if (
+        Math.abs(answers[i][0] - answers1[i * 2][0]) < eps * 3 ||
+        Math.abs(answers[i][1] - answers1[i * 2][1]) < eps * 3
+      ) {
+        return { flag: true };
       }
     }
+    return { flag: false };
   }
 
   function getY2(x, y1, y) {
-    return [y1, (x + y1 + y) / 3];
+    // return [y1, (Math.E ** x + y + y1) / 3];
+    return [y1, Math.sin(x * y) + y1];
   }
 
   function arraySum(first, second) {
@@ -104,84 +119,8 @@ function App() {
     return y;
   }
 
-  // const ryngePr = () => {
-  //   console.group(
-  //     '%cМетод Рунге-Кутта с усреднением по производной',
-  //     'font-weight: bold; color: red; text-transform: uppercase'
-  //   );
-
-  //   const table = [[x0, y0]];
-  //   const answers = [];
-  //   const vych = [];
-
-  //   for (let i = 0; i < hCount; i++) {
-  //     const funcValue = getValue(table[i][0], table[i][1]);
-  //     const yPol = table[i][1] + (h / 2) * funcValue;
-  //     const funcValue1 = getValue(table[i][0] + h, yPol);
-  //     const y = table[i][1] + (h / 2) * (funcValue + funcValue1);
-  //     table[i + 1] = [table[i][0] + h, y];
-  //     answers.push(y);
-  //     vych.push({ funcValue: funcValue, yPol: yPol, newFuncValue: funcValue1, y: y });
-  //   }
-
-  //   console.log('Answers: ', answers);
-  //   console.log('Table: ', answers);
-  //   console.table(vych);
-  //   console.groupEnd();
-  // };
-
-  // const eiler = () => {
-  //   console.group('%cМетод Эйлера', 'font-weight: bold; color: red; text-transform: uppercase');
-  //   const answers = [];
-  //   const table = [[x0, y0]];
-  //   const vych = [];
-  //   for (let i = 0; i < hCount; i++) {
-  //     const funcValue = getValue(table[i][0], table[i][1]);
-  //     const y = table[i][1] + funcValue * h;
-  //     const x = table[i][0] + h;
-  //     table[i + 1] = [x, y];
-  //     answers.push(y);
-  //     vych.push({ funcValue: funcValue, y: y });
-  //   }
-
-  //   console.log('table: ', table);
-  //   console.log('Answers: ', answers);
-  //   console.table(vych);
-  //   console.groupEnd();
-  // };
-
-  // const ryngeTime = () => {
-  //   console.group(
-  //     '%cМетод Рунге-Кутта с усреднением по времени',
-  //     'font-weight: bold; color: red; text-transform: uppercase'
-  //   );
-
-  //   const table = [[x0, y0]];
-  //   const answers = [];
-  //   const vych = [];
-
-  //   for (let i = 0; i < hCount; i++) {
-  //     const funcValue = getValue(table[i][0], table[i][1]);
-  //     const yPol = table[i][1] + (h / 2) * funcValue;
-  //     const funcValue1 = getValue(table[i][0] + h / 2, yPol);
-  //     const y = table[i][1] + h * funcValue1;
-  //     table[i + 1] = [table[i][0] + h, y];
-  //     answers.push(y);
-  //     vych.push({ funcValue: funcValue, yPol: yPol, newFuncValue: funcValue1, y: y });
-  //   }
-
-  //   console.log('Answers: ', answers);
-  //   console.log('Table: ', table);
-  //   console.table(vych);
-
-  //   console.groupEnd();
-  // };
-
   useEffect(() => {
-    console.log('Answer: ', rengeKytta2());
-    // eiler();
-    // ryngeTime();
-    // ryngePr();
+    rengeKytta2();
   }, []);
 
   return <div className="App"></div>;
